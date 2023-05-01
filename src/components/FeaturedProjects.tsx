c189199b6e52b5e5b529dba6f6235c6cd1cb41dc
chris <alihussainirfan575@gmail.com>
2023-05-01 11:50:20 -0500

    changes

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -1,34 +1,30 @@
-import { getFeaturedProjects } from "../lib/fetchers";
-import ButtonLink from "./ButtonLink";
-import FadeUp from "./FadeUp";
-import ProjectsMasonry from "./ProjectsMasonry";
+import { getAllProjects, getFeaturedProjects } from '../lib/fetchers';
+import ButtonLink from './ButtonLink';
+import FadeUp from './FadeUp';
+import ProjectsMasonry from './ProjectsMasonry';
 
 const FeaturedProjects = async () => {
-	const projects = await getFeaturedProjects();
+  const projects = await getAllProjects();
 
-	return (
-		<section className="max-w-2xl mt-20 mx-auto md:max-w-none md:mt-28" id="projects">
-			<ProjectsMasonry
-				projects={projects}
-				className="flex-row-reverse"
-				FirstItem={
-					<div>
-						<FadeUp cascade damping={0.15}>
-							<h2 className="h2">Featured projects</h2>
-							<p>Here are some of my projects that I've worked on:</p>
-						</FadeUp>
-					</div>
-				}
-				LastItem={
-					<FadeUp>
-						<ButtonLink href="/projects" variant="primary">
-							See more projects
-						</ButtonLink>
-					</FadeUp>
-				}
-			/>
-		</section>
-	);
+  return (
+    <section
+      className="max-w-2xl mt-20 mx-auto md:max-w-none md:mt-28"
+      id="projects"
+    >
+      <ProjectsMasonry
+        projects={projects}
+        className="flex-row-reverse"
+        FirstItem={
+          <div>
+            <FadeUp cascade damping={0.15}>
+              <h2 className="h2">All projects</h2>
+              <p>Here are some of my projects that I've worked on:</p>
+            </FadeUp>
+          </div>
+        }	
+      />
+    </section>
+  );
 };
 
 export default FeaturedProjects as unknown as () => JSX.Element;
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,58 +1,47 @@
-import Image from "next/image";
-import SanityText from "./SanityText";
-import { BsFillFilePdfFill, BsGithub, BsLinkedin } from "react-icons/bs";
-import ButtonLink from "./ButtonLink";
-import FlexSection from "./FlexSection";
-import FlexSectionImage from "./FlexSectionImage";
-import FlexSectionText from "./FlexSectionText";
-import SocialLink from "./SocialLink";
-import { getIntro } from "../lib/fetchers";
-import ParticleCanvas from "./Canvas";
+import Image from 'next/image';
+import SanityText from './SanityText';
+import { BsFillFilePdfFill, BsGithub, BsLinkedin } from 'react-icons/bs';
+import ButtonLink from './ButtonLink';
+import FlexSection from './FlexSection';
+import FlexSectionImage from './FlexSectionImage';
+import FlexSectionText from './FlexSectionText';
+import SocialLink from './SocialLink';
+import { getIntro } from '../lib/fetchers';
+import ParticleCanvas from './Canvas';
 
 const Intro = async () => {
-	const { largeText, smallText } = await getIntro();
+  const { largeText, smallText } = await getIntro();
 
-	return (
-		<FlexSection>
-			<FlexSectionText className="relative z-40" >
-				<h2 className="h2 mb-0">Hello!</h2>
-				<h1 className="-ml-1 mt-1 font-bold text-6xl">I am Xavier Johnson</h1>
-				<SanityText className="mt-8 text-xl" content={largeText} />
-				<SanityText className="mt-3" content={smallText} />
-				<div className="flex mt-8 space-x-4 font-medium">
-					<ButtonLink href="/projects" variant="primary">
-						View projects
-					</ButtonLink>
-					<ButtonLink
-						href="mailto:kovach.daniil.js@gmail.com"
-						variant="secondary"
-					>
-						Contact me
-					</ButtonLink>
-				</div>
-				<div className="flex space-x-6 mt-6 text-sm">
-					<SocialLink
-						href="https://github.com/K0D0D"
-						title="GitHub"
-						Icon={<BsGithub />}
-					/>
-					<SocialLink
-						href="https://linkedin.com/in/daniil-kovach-k0d0d"
-						title="LinkedIn"
-						Icon={<BsLinkedin />}
-					/>
-					<SocialLink
-						href="https://drive.google.com/file/d/1ofasGYjbqz2VZpdXdyxBp-BGPOiBjShH/view"
-						title="Resume"
-						Icon={<BsFillFilePdfFill />}
-					/>
-				</div>
-			</FlexSectionText>
-			<FlexSectionImage className="flex-1 md:pb-[35%] z-40 ">
-				<Image src="/intro.svg" priority fill alt="intro" />
-			</FlexSectionImage>
-		</FlexSection>
-	);
+  return (
+    <FlexSection>
+      <FlexSectionText className="relative z-40">
+        <h2 className="h2 mb-0">Hello!</h2>
+        <h1 className="-ml-1 mt-1 font-bold text-6xl">I am Xavier Johnson</h1>
+        <SanityText className="mt-8 text-xl" content={largeText} />
+        <SanityText className="mt-3" content={smallText} />
+        <div className="flex space-x-6 mt-6 text-sm">
+          <SocialLink
+            href="https://github.com/K0D0D"
+            title="GitHub"
+            Icon={<BsGithub />}
+          />
+          <SocialLink
+            href="https://linkedin.com/in/daniil-kovach-k0d0d"
+            title="LinkedIn"
+            Icon={<BsLinkedin />}
+          />
+          <SocialLink
+            href="https://drive.google.com/file/d/1ofasGYjbqz2VZpdXdyxBp-BGPOiBjShH/view"
+            title="Resume"
+            Icon={<BsFillFilePdfFill />}
+          />
+        </div>
+      </FlexSectionText>
+      <FlexSectionImage className="flex-1 md:pb-[35%] z-40 ">
+        <Image src="/intro.svg" priority fill alt="intro" />
+      </FlexSectionImage>
+    </FlexSection>
+  );
 };
 
 export default Intro as unknown as () => JSX.Element; //ts server component workaround
